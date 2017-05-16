e20f3bafa1e42e612a0f3e772efa9fd51a45f877
Avinash Chauhan <avinash664591@gmail.com>
2017-05-17 00:20:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+//this is a script file and no need to write script tags ,below i have declared and assigned age,name etc using "var" keyword
+var Name= "Avinash";
+var Age=25;
+var Date_of_birth="07/02/1992";
+var Place_of_birth="India";
+console.log("My Name is : "+Name ,",", "My Age is : "+Age ,",", "My Date of birth is : "+Date_of_birth,"&","place of birth is : "+Place_of_birth);
